1f5210beff6d06e24b98d328194526a13249d2c9
Steven Petryk <steven.petryk@intercom.io>
2019-06-08 18:55:43 -0700

    Fix bucket credentials

--- backend.tf
+++ backend.tf
@@ -1,7 +1,5 @@
-data "terraform_remote_state" "network" {
-  backend = "s3"
-
-  config = {
+terraform {
+  backend "s3" {
     bucket = "stevenpetryk-terraform-state"
     key    = "terraform.tfstate"
     region = "us-east-1"
